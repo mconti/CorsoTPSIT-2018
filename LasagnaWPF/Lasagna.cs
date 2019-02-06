c679d7fb6878dd748478b36e5df69136a74ab506
mconti <posta@maurizioconti.com>
2019-02-06 17:25:49 +0100

    Primo Commit

---
+++ LasagnaWPF/Lasagna.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LasagnaWPF
+{
+    public class Lasagna
+    {
+        public string Nome { get; set; }
+        public double Peso { get; set; }
+        public double Prezzo { get; set; }
+        public Lasagna(){}
+
+        public Lasagna (string riga)
+        {
+            string[] colonne = riga.Split(';');
+
+            //double peso;
+            double.TryParse(colonne[1], out double peso);
+            double.TryParse(colonne[2], out double prezzo);
+
+            Nome = colonne[0];
+            Peso = peso;
+            Prezzo = prezzo;
+        }
+
+    }
+
+    public class Lasagne : List<Lasagna>
+    {
+        public Lasagne() { }
+        public Lasagne( string nomeFile )
+        {
+            StreamReader fin = new StreamReader(nomeFile);
+            int quanteColonne = fin.ReadLine().Split(';').Length;
+            if (quanteColonne == 3)
+            {
+                while (!fin.EndOfStream)
+                {
+                    string riga = fin.ReadLine();
+                    Add(new Lasagna(riga));
+                }
+            }
+            else
+                throw new Exception("No No !!! Non si fa!!!");
+        }
+    }
+}
---
+++ LasagnaWPF/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LasagnaWPF
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            btnApri.Click += Pippo;
+            btnApri.Click += Pluto;
+        }
+
+        private void Pluto(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Eccolo");
+        }
+
+        private void Pippo(object sender, RoutedEventArgs e)
+        {
+            List<Lasagna> lasagne = new List<Lasagna>();
+
+            StreamReader fin = new StreamReader("in.csv");
+            fin.ReadLine();
+
+            while(!fin.EndOfStream)
+            {
+                string riga = fin.ReadLine();
+                string[] colonne = riga.Split(';');
+
+                //double peso;
+                double.TryParse(colonne[1], out double peso);
+                double.TryParse(colonne[2], out double prezzo);
+
+                Lasagna l = new Lasagna
+                {
+                    Nome = colonne[0],
+                    Peso = peso,
+                    Prezzo = prezzo
+                };
+
+                lasagne.Add(l);
+            }
+            dgDati.ItemsSource = lasagne;
+
+        }
+
+        private void BtnApri2_Click(object sender, RoutedEventArgs e)
+        {
+            List<Lasagna> lasagne = new List<Lasagna>();
+
+            StreamReader fin = new StreamReader("in.csv");
+            fin.ReadLine();
+
+            while (!fin.EndOfStream)
+            {
+                string riga = fin.ReadLine();
+                lasagne.Add(new Lasagna(riga));
+            }
+            dgDati.ItemsSource = lasagne;
+        }
+
+        private void BtnApri3_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                dgDati.ItemsSource = new Lasagne("in.csv");
+            }
+            catch (Exception erore)
+            {
+                MessageBox.Show(erore.Message);
+            }
+        }
+    }
+}
