eeb67fe7a74f87d894de27b295927baccc872e0a
mconti <posta@maurizioconti.com>
2019-02-09 23:29:43 +0100

    Aggiunto progetto Cross platform

---
+++ LasagnaCross/LasagnaCross/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LasagnaCross
+{
+    
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnApri_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                lvDati1.ItemsSource = new LasagnaModel.Lasagne(@"Data\in.csv");
+                lvDati2.ItemsSource = new LasagnaModel.Lasagne(@"Data\in.csv");
+            }
+            catch (Exception erore)
+            {
+                DisplayAlert("Errore", erore.Message, "OK");
+            }
+        }
+    }
+}
---
+++ LasagnaCross/LasagnaCross/Models/Lasagna.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LasagnaModel
+{
+    public class Lasagna
+    {
+        public string Nome { get; set; }
+        public double Peso { get; set; }
+        public double Prezzo { get; set; }
+        public Lasagna() { }
+
+        public Lasagna(string riga)
+        {
+            string[] colonne = riga.Split(';');
+
+            //double peso;
+            double.TryParse(colonne[1], out double peso);
+            double.TryParse(colonne[2], out double prezzo);
+
+            Nome = colonne[0];
+            Peso = peso;
+            Prezzo = prezzo;
+        }
+
+    }
+
+    public class Lasagne : List<Lasagna>
+    {
+        public Lasagne() { }
+        public Lasagne(string nomeFile)
+        {
+            var realFile = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), nomeFile );
+            if (realFile == null || !File.Exists(realFile))
+            {
+                using (var fout = File.CreateText(realFile))
+                {
+                    fout.WriteLineAsync("Nome;Prezzo;Peso");
+                    fout.WriteLineAsync("Lasagne alla Bolognese;1,2;100");
+                }
+            }
+        
+
+            StreamReader fin = new StreamReader(realFile);
+            int quanteColonne = fin.ReadLine().Split(';').Length;
+            if (quanteColonne == 3)
+            {
+                while (!fin.EndOfStream)
+                {
+                    string riga = fin.ReadLine();
+                    Add(new Lasagna(riga));
+                }
+            }
+            else
+                throw new Exception("No No !!! Non si fa!!!");
+        }
+    }
+}
